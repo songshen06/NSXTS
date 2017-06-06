55a380a86dc0940f2ad0495b8e8e08f93aeaf248
root <songshen06@gmail.com>
2017-06-06 11:33:31 +0800

    Add send '\n'

--- GetRouteTable.py
+++ GetRouteTable.py
@@ -5,22 +5,22 @@ import time
 import re
 from ConfigParser import ConfigParser
 def GetEdgeList(nsxm_host,nsxm_user,nsxm_pass):
-	ssh = paramiko.SSHClient()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	ssh.connect(nsxm_host,22,nsxm_user,nsxm_pass)
-	ssh_conn = ssh.invoke_shell()
-	print "=============connected to nsxmanager %s" %nsxm_host
-	time.sleep(1)
-	ssh_conn.send('show edge all\n')
-	time.sleep(1)
-	output = ssh_conn.recv(10000)
-	print output
-	edge_Id_list = []
-	for x in output.splitlines():
-		if 'edge-' in x:
-			edge_Id_list = [re.split('\s*',x)[0]] + edge_Id_list
-	command_route_list = ['show edge '+x + ' ip route\n' for x in edge_Id_list]
-	for command in command_route_list:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(nsxm_host,22,nsxm_user,nsxm_pass)
+        ssh_conn = ssh.invoke_shell()
+        print "=============connected to nsxmanager %s" %nsxm_host
+        time.sleep(1)
+        ssh_conn.send('show edge all\n')
+        time.sleep(1)
+        output = ssh_conn.recv(10000)
+        print output
+        edge_Id_list = []
+        for x in output.splitlines():
+                if 'edge-' in x:
+                        edge_Id_list = [re.split('\s*',x)[0]] + edge_Id_list
+        command_route_list = ['show edge '+x + ' ip route\n' for x in edge_Id_list]
+        for command in command_route_list:
                 #print command
                 ssh_conn.send(command)
                 time.sleep(2)
@@ -29,22 +29,22 @@ def GetEdgeList(nsxm_host,nsxm_user,nsxm_pass):
                 #print stdout.read()
         output = ssh_conn.recv(10000)
         timestr = time.strftime("%Y%m%d-%H%M%S")
-	print output
-	f = open(timestr,'w+')
-	f.write(output)
-	f.close()
-	ssh.close()
-		
-def main():	
-	filename = 'info.ini'
-	config = ConfigParser()
-	config.read(filename)
-	nsxm_user = config.get("nsxm","nsxm_user")
-	nsxm_host = config.get("nsxm","nsxm_host")
-	nsxm_pass = config.get("nsxm","nsxm_pass");
-	print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) 
-	
-	GetEdgeList(nsxm_host,nsxm_user,nsxm_pass)
-		
+        print output
+        f = open(timestr,'w+')
+        f.write(output)
+        f.close()
+        ssh.close()
+
+def main():
+        filename = 'info.ini'
+        config = ConfigParser()
+        config.read(filename)
+        nsxm_user = config.get("nsxm","nsxm_user")
+        nsxm_host = config.get("nsxm","nsxm_host")
+        nsxm_pass = config.get("nsxm","nsxm_pass");
+        print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+        GetEdgeList(nsxm_host,nsxm_user,nsxm_pass)
+
 if __name__ == "__main__":
    main()
